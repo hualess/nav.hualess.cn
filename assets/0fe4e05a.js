8b71418e54ce41597390f3e463b0006ba56083b9
hualess <hualess@users.noreply.github.com>
2024-09-14 15:50:48 +0800

    Deploying to main from  @ 9e1f4b8ebac1aaf74c41ff55bd512f6a11a88afe 🚀

---
+++ assets/0fe4e05a.js
@@ -0,0 +1,2 @@
+import{_ as o,p as s,k as _,c,t as n,y as p,o as a}from"./70bc3bac.js";const u={class:"jump-content"},r={__name:"AppJump",setup(i){const e=s(),t=atob(e.query.id);return _(()=>{setTimeout(()=>{window.location.href=t},2e3)}),(d,m)=>(a(),c("div",u,"页面即将跳转至："+n(p(t)),1))}},f=o(r,[["__scopeId","data-v-1c001651"]]);export{f as default};
+//# sourceMappingURL=0fe4e05a.js.map
